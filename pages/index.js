40c1e3b8a4568ba1d9cbacb9d2109ae0cefbfa82
Bruno <bno27_mb@outlook.com>
2026-03-04 02:40:21 -0300

    add novo html da página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 function Home() {
-  return <h1>Bora disco hj?</h1>;
+  return (
+    <main>
+      <h1>🚧 SITE EM DESENVOLVIMENTO 🛠️</h1>
+
+      <h2>
+        Estamos preparando um espaço dedicado às histórias, curiosidades e
+        tradições do Japão.
+      </h2>
+
+      <section>
+        <p>⚔️ Samurais e o código de honra Bushidō</p>
+        <p>🏹 Grandes guerreiros da era Sengoku</p>
+        <p>🏯 A vida no Japão durante o período Edo</p>
+        <p>🗡️ A história de lendas como Miyamoto Musashi</p>
+        <p>👘 Costumes e curiosidades da cultura japonesa antiga</p>
+        <p>🎎 Literatura clássica como o Conto de Genji</p>
+        <p>⛩️ Castelos, tradições e filosofia do Japão feudal</p>
+      </section>
+    </main>
+  );
 }
 
 export default Home;
